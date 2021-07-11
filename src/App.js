103ff3d1d7ff417a1cb64f1a100fe8cb0b5c285c
Subhiraj Singh <subhiraj.singh@cpaperless.com>
2021-07-12 00:54:03 +0530

    master commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [item, setItems] = useState({});
+  const [showPunchline, setShowPunchline] = useState(false);
+
+  useEffect(() => {
+    getRandomJoke();
+  }, [])
+
+
+  function getRandomJoke() {
+    fetch("https://official-joke-api.appspot.com/jokes/random")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setShowPunchline(false);
+          setIsLoaded(true);
+          setItems(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }
+
+
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div className="mainDiv">
+        <h2>Random Joke</h2>
+        <span>{item.setup}</span><br />
+        <button className={showPunchline ? "hide" : "show"} onClick={() => setShowPunchline(true)}>Show Punchline</button>
+        <div className={showPunchline ? "show" : "hide"}>{item.punchline}</div>
+        <br />
+        <button className="reload" onClick={() => getRandomJoke()}>New Joke &#x21bb;</button>
+      </div>
+    );
+  }
 }
 
 export default App;
